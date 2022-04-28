f6c9edf0a4f8629e6aa15f8b8fd88b4063355a26
Dat Nguyen <dat.nguyen@digicommercegroup.com>
2022-04-28 20:04:59 +0700

    Add

--- Django/main/views.py
+++ Django/main/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Main
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
